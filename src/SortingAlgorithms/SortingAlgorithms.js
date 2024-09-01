c8de5de8426b078c7b956b76e4cb0c6823412abc
Mini <a100567@alunos.uminho.pt>
2024-09-01 13:10:59 +0100

    quicksort visualizer implementation

---
+++ src/SortingAlgorithms/SortingAlgorithms.js
@@ -0,0 +1,113 @@
+export function getMergeSortAnimations(array) {
+    const animations = [];
+    if (array.length <= 1) return array;
+    const auxiliaryArray = array.slice();
+    mergeSortHelper(array, 0, array.length - 1, auxiliaryArray, animations);
+    return animations;
+  }
+
+  function mergeSortHelper(
+    mainArray,
+    startIdx,
+    endIdx,
+    auxiliaryArray,
+    animations,
+  ) {
+    if (startIdx === endIdx) return;
+    const middleIdx = Math.floor((startIdx + endIdx) / 2);
+    mergeSortHelper(auxiliaryArray, startIdx, middleIdx, mainArray, animations);
+    mergeSortHelper(auxiliaryArray, middleIdx + 1, endIdx, mainArray, animations);
+    doMerge(mainArray, startIdx, middleIdx, endIdx, auxiliaryArray, animations);
+  }
+
+  function doMerge(
+    mainArray,
+    startIdx,
+    middleIdx,
+    endIdx,
+    auxiliaryArray,
+    animations,
+  ) {
+    let k = startIdx;
+    let i = startIdx;
+    let j = middleIdx + 1;
+    while (i <= middleIdx && j <= endIdx) {
+      // These are the values that we're comparing; we push them once
+      // to change their color.
+      animations.push([i, j]);
+      // These are the values that we're comparing; we push them a second
+      // time to revert their color.
+      animations.push([i, j]);
+      if (auxiliaryArray[i] <= auxiliaryArray[j]) {
+        // We overwrite the value at index k in the original array with the
+        // value at index i in the auxiliary array.
+        animations.push([k, auxiliaryArray[i]]);
+        mainArray[k++] = auxiliaryArray[i++];
+      } else {
+        // We overwrite the value at index k in the original array with the
+        // value at index j in the auxiliary array.
+        animations.push([k, auxiliaryArray[j]]);
+        mainArray[k++] = auxiliaryArray[j++];
+      }
+    }
+    while (i <= middleIdx) {
+      // These are the values that we're comparing; we push them once
+      // to change their color.
+      animations.push([i, i]);
+      // These are the values that we're comparing; we push them a second
+      // time to revert their color.
+      animations.push([i, i]);
+      // We overwrite the value at index k in the original array with the
+      // value at index i in the auxiliary array.
+      animations.push([k, auxiliaryArray[i]]);
+      mainArray[k++] = auxiliaryArray[i++];
+    }
+    while (j <= endIdx) {
+      // These are the values that we're comparing; we push them once
+      // to change their color.
+      animations.push([j, j]);
+      // These are the values that we're comparing; we push them a second
+      // time to revert their color.
+      animations.push([j, j]);
+      // We overwrite the value at index k in the original array with the
+      // value at index j in the auxiliary array.
+      animations.push([k, auxiliaryArray[j]]);
+      mainArray[k++] = auxiliaryArray[j++];
+    }
+  }
+
+export function getQuickSortAnimations(array) {
+    const animations = [];
+    if (array.length <= 1) return array;
+    quickSortHelper(array, 0, array.length - 1, animations);
+    return animations;
+  }
+
+  function partition(array, startIdx, endIdx, animations) {
+    const pivot = array[endIdx];
+    let i = startIdx - 1;
+    for (let j = startIdx; j < endIdx; j++) {
+      if (array[j] <= pivot) {
+        i++;
+        animations.push([i, j]);
+        animations.push([i, j]);
+        animations.push([i, array[j]]);
+        animations.push([j, array[i]]);
+        [array[i], array[j]] = [array[j], array[i]];
+      }
+    }
+    animations.push([i + 1, endIdx]);
+    animations.push([i + 1, endIdx]);
+    animations.push([i + 1, array[endIdx]]);
+    animations.push([endIdx, array[i + 1]]);
+    [array[i + 1], array[endIdx]] = [array[endIdx], array[i + 1]];
+    return i + 1;
+  }
+
+  function quickSortHelper(array, startIdx, endIdx, animations) {
+    if (startIdx < endIdx) {
+      const pi = partition(array, startIdx, endIdx, animations);
+      quickSortHelper(array, startIdx, pi - 1, animations);
+      quickSortHelper(array, pi + 1, endIdx, animations);
+    }
+  }
